10da46e56ff45bc1d7695f5d8328dae5446b8f08
jplataquin <jp.lataquin@gmail.com>
2022-08-17 19:40:41 +0800

    test

--- resources/views/payment/creditcard.blade.php
+++ resources/views/payment/creditcard.blade.php
@@ -2,106 +2,145 @@
 
 @section('content')
 
-<div class="container">
-    <h3>Credit Card Details</h3>
-    <div class="row mb-3">
-        <div class="col">
-            <div class="form-group">
-                <label>Name of Holder</label>
-                <input type="text" id="name" class="form-control"/>
+<div id="mainContainer" class="container">
+    <div id="formContainer">
+        <h3>Credit Card Details</h3>
+        <div class="row mb-3">
+            <div class="col">
+                <div class="form-group">
+                    <label>Name of Holder</label>
+                    <input type="text" id="name" class="form-control"/>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="row mb-3">
-        <div class="col">
-            <div class="form-group">
-                <label>Card No</label>
-                <input type="text" id="ccno" class="form-control"/>
+        <div class="row mb-3">
+            <div class="col">
+                <div class="form-group">
+                    <label>Card No</label>
+                    <input type="text" id="ccno" class="form-control"/>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="row mb-3">
-        <div class="col">
-            <div class="form-group">
-                <label>Expiry Date</label>
-                <input type="text" id="expiry" placeholder="MM/YY" class="form-control"/>
+        <div class="row mb-3">
+            <div class="col">
+                <div class="form-group">
+                    <label>Expiry Date</label>
+                    <input type="text" id="expiry" placeholder="MM/YY" class="form-control"/>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="row mb-3">
-        <div class="col">
-            <div class="form-group">
-                <label>CVC</label>
-                <input type="text" id="cvc" class="form-control"/>
+        <div class="row mb-3">
+            <div class="col">
+                <div class="form-group">
+                    <label>CVC</label>
+                    <input type="text" id="cvc" class="form-control"/>
+                </div>
             </div>
         </div>
-    </div>
 
-    <h3>Billing Address</h3>
-    <div class="row mb-3">
-        <div class="col">
-            <div class="form-group">
-                <label>Address Line 1</label>
-                <input type="text" id="address_line_1" class="form-control"/>
+        <h3>Billing Address</h3>
+        <div class="row mb-3">
+            <div class="col">
+                <div class="form-group">
+                    <label>Email</label>
+                    <input type="text" id="email" class="form-control"/>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="row mb-3">
-        <div class="col">
-            <div class="form-group">
-                <label>Address Line 2</label>
-                <input type="text" id="address_line_2" class="form-control"/>
+        <div class="row mb-3">
+            <div class="col">
+                <div class="form-group">
+                    <label>Phone/Mobile No:</label>
+                    <input type="text" id="phone" class="form-control"/>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="row mb-3">
-        <div class="col">
-            <div class="form-group">
-                <label>City</label>
-                <input type="text" id="city" class="form-control"/>
+        <div class="row mb-3">
+            <div class="col">
+                <div class="form-group">
+                    <label>Address Line 1</label>
+                    <input type="text" id="address_line_1" class="form-control"/>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="row mb-3">
-        <div class="col">
-            <div class="form-group">
-                <label>State / Province</label>
-                <input type="text" id="state_province" class="form-control"/>
+        <div class="row mb-3">
+            <div class="col">
+                <div class="form-group">
+                    <label>Address Line 2</label>
+                    <input type="text" id="address_line_2" class="form-control"/>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="row mb-3">
-        <div class="col">
-            <div class="form-group">
-                <label>Postal / Zip Code</label>
-                <input type="text" id="postal_zip_code" class="form-control"/>
+        <div class="row mb-3">
+            <div class="col">
+                <div class="form-group">
+                    <label>City</label>
+                    <input type="text" id="city" class="form-control"/>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="row mb-3">
-        <div class="col">
-            <div class="form-group">
-                <label>Country</label>
-                <select class="form-control" id="country">
-                    @foreach(config('selectoptions')['countries'] as $key => $text)
-                        <option value="{{$key}}">{{$text}}</option>
-                    @endforeach
-                </select>
+        <div class="row mb-3">
+            <div class="col">
+                <div class="form-group">
+                    <label>State / Province</label>
+                    <input type="text" id="state_province" class="form-control"/>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="row mb-3">
-        <div class="col text-end">
-            <button id="payBtn" class="btn btn-primary">Pay Now</button>
+        <div class="row mb-3">
+            <div class="col">
+                <div class="form-group">
+                    <label>Postal / Zip Code</label>
+                    <input type="text" id="postal_zip_code" class="form-control"/>
+                </div>
+            </div>
+        </div>
+        <div class="row mb-3">
+            <div class="col">
+                <div class="form-group">
+                    <label>Country</label>
+                    <select class="form-control" id="country">
+                        @foreach(config('selectoptions')['countries'] as $key => $text)
+                            <option value="{{$key}}">{{$text}}</option>
+                        @endforeach
+                    </select>
+                </div>
+            </div>
+        </div>
+        <div class="row mb-3">
+            <div class="col text-end">
+                <button id="payBtn" class="btn btn-primary">Pay Now</button>
+            </div>
         </div>
     </div>
 </div>
 <script type="module">
-    const name      = document.querySelector('#name');
-    const ccno      = document.querySelector('#ccno');
-    const expiry    = document.querySelector('#expiry');
-    const cvc       = document.querySelector('#cvc');
-    const payBtn    = document.querySelector('#payBtn');
+    import {Template} from '/adarna.js';
+
+    const name              = document.querySelector('#name');
+    const ccno              = document.querySelector('#ccno');
+    const expiry            = document.querySelector('#expiry');
+    const cvc               = document.querySelector('#cvc');
+    const line_address_1    = document.querySelector('#line_address_1');
+    const line_address_2    = document.querySelector('#line_address_2');
+    const state_province    = document.querySelector('#state_province');
+    const city              = document.querySelector('#city');
+    const country           = document.querySelector('#country');
+    const postal_zip_code   = document.querySelector('#postal_zip_code');
+    const email             = document.querySelector('#email');
+    const phone             = document.querySelector('#phone');
+    const payBtn            = document.querySelector('#payBtn');
+    const formContainer     = document.querySelector('#formContainer');
+    const mainContainer     = document.querySelector('#mainContainer');
+
+    const t         = new Template();
+    const iframe    = t.iframe({
+        style:{
+            width:'100%',
+            height:'100%',
+            border:'none'
+        }
+    });
 
     expiry.onkeydown = (e)=>{
         
@@ -138,23 +177,16 @@
     }
 
     payBtn.onclick = (e)=>{
+       
         e.preventDefault();
-        
-        /**
-        const formData = new FormData();
+        payBtn.disabled = true;
 
-        formData.append('name',name.value);
-        formData.append('ccno',ccno.value);
-        formData.append('expiry',expiry.value);
-        formData.append('ccv',ccv.value);
-        formData.append('amount',1000);
-        **/
+        //TODO VALIDATE DETAILS
 
         let exp = expiry.value.split('/');
 
         window.util.$post('/payment/creditcard').then(reply=>{
 
-        
             let clientKey       = reply.data.clientKey;
             let key             = reply.data.key;
 
@@ -163,26 +195,24 @@
                     'attributes':{
                         type:'card',
                         details:{
-                            card_number:ccno.value,
-                            exp_month: parseInt(exp[0]),
-                            exp_year: parseInt(exp[1]),
-                            cvc: cvc.value
+                            card_number :ccno.value,
+                            exp_month   : parseInt(exp[0]),
+                            exp_year    : parseInt(exp[1]),
+                            cvc         : cvc.value
                         },
                         billing:{
                             address:{
-                                line1:'address 1',
-                                line2:'address 2',
-                                city:'city',
-                                state:'state',
-                                postal_code:'postal_code',
-                                country:'PH'
+                                line1       :line_address_1.value,
+                                line2       :line_address_2.value,
+                                city        :city.value,
+                                state       :state_province.value,
+                                postal_code :postal_zip_code.value,
+                                country     :country.value
                             },
-                            name:'JasdadP',
-                            email:'jp.lataquin@gmail.com',
-                            phone:'09088189764',
-                            metadata:{
-                                hello:'world'
-                            }
+                            name    : name.value,
+                            email   : email.value,
+                            phone   : phone.value,
+                            metadata:{}
                         }
                     }
                 }
@@ -190,14 +220,29 @@
 
                 let paymentMethodId = response.data.id;
                 
-
-                console.log('payment method',response);
-                
-                return wtf(paymentMethodId,clientKey,key);
+                return monitor(paymentMethodId,clientKey,key);
             });
         });
     }
 
+    function validate(){
+        console.log('validate');
+    }
+
+    function showIframe(url){
+        formContainer.style.display = 'none';
+        iframe.src                  = url;
+
+        mainContainer.append(iframe);
+    }
+
+    function success(){
+        console.log('Success');
+    }
+
+    function failed(){
+        console.log('Failed');
+    }
 
     function paymentMethod(key,data){
         
@@ -217,7 +262,7 @@
         .then(response => { return response.json(); });
     }
 
-    function wtf(paymentMethodId,clientKey,key){
+    function monitor(paymentMethodId,clientKey,key){
 
         // Get the payment intent id from the client key
         let paymentIntentId = clientKey.split('_client')[0];
@@ -243,15 +288,31 @@
             console.log(paymentIntent);
             console.log(paymentIntentStatus);
             console.log(paymentIntent.attributes.next_action);
+
             if (paymentIntentStatus === 'awaiting_next_action') {
                 // Render your modal for 3D Secure Authentication since next_action has a value. You can access the next action via paymentIntent.attributes.next_action.
+                //paymentIntent.attributes.next_action
+                showIframe('https://google.com');
+
             } else if (paymentIntentStatus === 'succeeded') {
                 // You already received your customer's payment. You can show a success message from this condition.
+                success(paymentMethodId,paymentIntentId);
+
             } else if(paymentIntentStatus === 'awaiting_payment_method') {
                 // The PaymentIntent encountered a processing error. You can refer to paymentIntent.attributes.last_payment_error to check the error and render the appropriate error message.
-            }  else if (paymentIntentStatus === 'processing'){
+                failed(paymentIntent.attributes.last_payment_error);
+                
+            } else if (paymentIntentStatus === 'processing'){
                 // You need to requery the PaymentIntent after a second or two. This is a transitory status and should resolve to `succeeded` or `awaiting_payment_method` quickly.
+                setTimeout(()=>{
+                    monitor(paymentMethodId,clientKey,key);
+                },2000);
+            }else{
+                console.log('Unknown status');
             }
+            
+        }).catch(err=>{
+
         });
 
     }
