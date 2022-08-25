23cbd8a119de2405d40265e3175df76df1b3a251
jplataquin <jp.lataquin@gmail.com>
2022-08-26 01:46:02 +0800

    test

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Item;
+
 class Brand extends Model
 {   
     protected $connection = 'mysql2';
@@ -12,7 +12,7 @@ class Brand extends Model
     use HasFactory;
 
     public function items(){
-        return $this->hasMany(Item::class,'brand_id','id');
+        return $this->hasMany('Items','brand_id','id');
     }
     
 }
