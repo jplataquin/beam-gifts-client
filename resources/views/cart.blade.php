83a9633516390907bfa76ee6cfb8eace63d6c0aa
jplataquin <jp.lataquin@gmail.com>
2022-08-25 22:26:13 +0800

    test

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -6,10 +6,10 @@
 
         @foreach($items as $item)
             <div class="row mb-3">
-                <div class="col">
+                <div class="col-2">
                     <img class="img" src="{{config('app')['api_base_url']}}storage/photos/item/150px/{{$item->attributes['image']}}"/>
                 </div>
-                <div class="col">
+                <div class="col-10">
                     <h3>{{$item->name}}</h3>
                     Qty: {{$item->quantity}}
                 </div>
