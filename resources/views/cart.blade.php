ac7f7f47bfccf467f48ae7ae54dd99e33f71901d
jplataquin <jp.lataquin@gmail.com>
2022-08-25 22:25:38 +0800

    test

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -5,9 +5,14 @@
     <div class="container">
 
         @foreach($items as $item)
-            <div>
-                <h3>{{$item->name}}</h3>
-                Qty: {{$item->quantity}}
+            <div class="row mb-3">
+                <div class="col">
+                    <img class="img" src="{{config('app')['api_base_url']}}storage/photos/item/150px/{{$item->attributes['image']}}"/>
+                </div>
+                <div class="col">
+                    <h3>{{$item->name}}</h3>
+                    Qty: {{$item->quantity}}
+                </div>
             </div>
         @endforeach
 
