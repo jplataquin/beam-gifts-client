4bde6af2cd59950a11042800681cd305201553a3
jplataquin <jp.lataquin@gmail.com>
2022-08-31 20:23:43 +0800

    test

--- resources/views/qr_display.blade.php
+++ resources/views/qr_display.blade.php
@@ -3,9 +3,11 @@
 @section('content')
 
 <div>
-    <div>
+    <div class="text-center">
         <img class="img" src="{{config('app')['api_base_url']}}storage/photos/item/400px/{{$photo['400px']}}"/>
+        <h2>{{$item->item_name}}</h2>
     </div>
+
 </div>
 
 @endsection
