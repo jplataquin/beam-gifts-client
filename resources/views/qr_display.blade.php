b755706f5b1d48acb78f4d9f5feca66ed616250e
jplataquin <jp.lataquin@gmail.com>
2022-08-31 22:41:15 +0800

    test

--- resources/views/qr_display.blade.php
+++ resources/views/qr_display.blade.php
@@ -3,30 +3,16 @@
 @section('content')
 
 <div class="row">
-    <div class="col">
+    <div class="col pt-3">
             <div>
-                <h3>A gift from {{$user->name}}</h3>
+                <h3>A gift from</h3>
+                <h3>{{$user->name}}</h3>
             </div>
 
             <div class="text-center">
                 <img class="img mb-3" width="100%" src="{{config('app')['api_base_url']}}storage/photos/item/400px/{{$photo['400px']}}"/>
                 <h2 class="mt-3 mb-3">{{$item->item_name}}</h2>
-                
-
-                <div class="card mb-3" style="max-width: 540px;">
-                    <div class="row g-0">
-                        <div class="col-sm-1">
-                        <img width="50px" src="{{config('app')['api_base_url']}}storage/photos/item/150px/{{$photo['150px']}}" class="img-fluid rounded-start" alt="...">
-                        </div>
-                        <div class="col-sm-11">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            
             </div>
 
             <div>
