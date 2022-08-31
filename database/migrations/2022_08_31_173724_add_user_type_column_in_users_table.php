de3714c79ce9c2592a4ffcc25c6839e37554a619
jplataquin <jp.lataquin@gmail.com>
2022-09-01 01:40:36 +0800

    test

---
+++ database/migrations/2022_08_31_173724_add_user_type_column_in_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $this->char('type',4)->default('CLNT')->after('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $this->dropColumn('type');
+        });
+    }
+};
