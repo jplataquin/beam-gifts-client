82e65bfcf4f13250f5c0521d964a17c58c7e4568
jplataquin <jp.lataquin@gmail.com>
2022-08-31 23:41:34 +0800

    test

--- resources/views/qr_display.blade.php
+++ resources/views/qr_display.blade.php
@@ -5,7 +5,7 @@
 <div class="row">
     <div class="col pt-3">
             <div class="border border-primary mb-3 p-3">
-                
+
                 <h3>A gift from</h3>
                 <h3>{{$user->name}}</h3>
             </div>
@@ -49,6 +49,12 @@
                         },
                     });
                 </script>
+
+                <h3>Scan at {{$item->brand_name}} to claim</h3>
+            </div>
+
+            <div>
+
             </div>
     </div>
 
