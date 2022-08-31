42f6fcc5bed501553a6e25bdc7e5a70ae0610c18
jplataquin <jp.lataquin@gmail.com>
2022-08-31 23:44:02 +0800

    test

--- resources/views/qr_display.blade.php
+++ resources/views/qr_display.blade.php
@@ -52,7 +52,7 @@
 
             </div>
 
-            <div class="border border-primary mb-3"> 
+            <div class="text-center border border-primary mb-3"> 
                 <h2 class="mt-3 mb-3">{{$item->consumed}} / {{$item->quantity}}</h2>
             </div>
     </div>
