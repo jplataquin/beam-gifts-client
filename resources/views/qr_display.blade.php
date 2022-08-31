3fe5588ed605b883ab3697d2f660f6af2d79a57a
jplataquin <jp.lataquin@gmail.com>
2022-08-31 20:42:21 +0800

    test

--- resources/views/qr_display.blade.php
+++ resources/views/qr_display.blade.php
@@ -2,44 +2,47 @@
 
 @section('content')
 
-<div class="p-3">
-    <div class="text-center">
-        <img class="img mb-3" src="{{config('app')['api_base_url']}}storage/photos/item/400px/{{$photo['400px']}}"/>
-        <h2>{{$item->item_name}}</h2>
+<div class="row">
+    <div class="col">
+            <div class="text-center">
+                <img class="img mb-3" src="{{config('app')['api_base_url']}}storage/photos/item/400px/{{$photo['400px']}}"/>
+                <h2>{{$item->item_name}}</h2>
+            </div>
+
+            <div>
+
+                <div class="text-center" id="qr_logo"></div>
+                <script type="module">
+                    import '/easyqrcode.js';
+
+                    new QRCode(document.getElementById('qr_logo'), {			
+                        text: JSON.stringify({
+                            item_uid: '{{$item->item_uid}}'
+                        }),
+                        
+                        width: 400,
+                        height: 400,
+                        colorDark: "#000000",
+                
+                        PI: '#00008F',
+                
+                        correctLevel: QRCode.CorrectLevel.H, // L, M, Q, H
+                
+                        //logo: 'logo.png',
+                        logoWidth: 43, // fixed logo width. default is `width/3.5`
+                        logoHeight: 43,
+                        autoColor: true,
+                    
+                        dotScale: 0.7,
+                        drawer: 'canvas',
+                        onRenderingEnd: function(qrCodeOptions) {
+                            
+                        },
+                    });
+                </script>
+            </div>
     </div>
 
-    <div>
-
-        <div class="text-center" id="qr_logo"></div>
-        <script type="module">
-            import '/easyqrcode.js';
-
-            new QRCode(document.getElementById('qr_logo'), {			
-				text: JSON.stringify({
-                    item_uid: '{{$item->item_uid}}'
-                }),
-				
-				width: 400,
-				height: 400,
-				colorDark: "#000000",
-		
-				PI: '#00008F',
-		
-				correctLevel: QRCode.CorrectLevel.H, // L, M, Q, H
-		
-				//logo: 'logo.png',
-				logoWidth: 43, // fixed logo width. default is `width/3.5`
-				logoHeight: 43,
-				autoColor: true,
-			
-				dotScale: 0.7,
-				drawer: 'canvas',
-				onRenderingEnd: function(qrCodeOptions) {
-					
-				},
-			});
-        </script>
-    </div>
 </div>
 
 @endsection
