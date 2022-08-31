02094319dc8aa2e903c5bab5f4516a8d8ffda339
jplataquin <jp.lataquin@gmail.com>
2022-08-31 22:39:06 +0800

    test

--- resources/views/qr_display.blade.php
+++ resources/views/qr_display.blade.php
@@ -15,15 +15,15 @@
 
                 <div class="card mb-3" style="max-width: 540px;">
                     <div class="row g-0">
-                        <div class="col-sm-2">
+                        <div class="col-sm-1">
                         <img width="50px" src="{{config('app')['api_base_url']}}storage/photos/item/150px/{{$photo['150px']}}" class="img-fluid rounded-start" alt="...">
                         </div>
-                        <div class="col-sm-10">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                        </div>
+                        <div class="col-sm-11">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            </div>
                         </div>
                     </div>
                 </div>
