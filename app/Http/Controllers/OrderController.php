d3b735e963365392a5a29d1f19c915fa29371e39
jplataquin <jp.lataquin@gmail.com>
2022-09-12 00:43:51 +0800

    test

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -73,7 +73,7 @@ class OrderController extends Controller
 
         $user_id = Auth::user()->id;
 
-        $orders = Order::where('user_id',$user_id)->results();
+        $orders = Order::where('user_id',$user_id)->get();
 
         return response()->json([
             'status' => 1,
