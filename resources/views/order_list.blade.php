2b37eb794d364d91514946bbb274dde16d802982
jplataquin <jp.lataquin@gmail.com>
2022-09-12 00:53:53 +0800

    test

--- resources/views/order_list.blade.php
+++ resources/views/order_list.blade.php
@@ -7,24 +7,52 @@
 
         <div id="list">
         </div>
+        <button id="showMoreBtn" class="btn btn-full btn-primary">Show More</button>
     </div>
 
     <script type="module">
         import {Template} from '/adarna.js';
 
-        const list = document.querySelector('#list');
+        let page            = 1;
+        const listEl        = document.querySelector('#list');
+        const showMoreBtn   = document.querySelector('#showMoreBtn');   
+        const t             = new Template();
 
-        window.util.$get('/api/myorders',{
-            page: 1,
-            limit: 10
-        }).then(reply=>{
+        function list(){
+            window.util.$get('/api/myorders',{
+                page: page,
+                limit: 10
+            }).then(reply=>{
 
-            if(!reply.status){
-                alert(reply.message);
-            }
-            
-        }).catch(e=>{
-            alert('Something went wrong');
-        });
+                if(!reply.status){
+                    alert(reply.message);
+                }
+                
+                reply.data.map(item=>{
+
+                    let el = t.div({class:'border border-parimary mb-3'},()=>{
+                        t.label(item.created_at);
+                        t.div(()=>{
+                            t.label('Order ref: '+item.uid);
+                        });
+                    });
+
+                    listEl.appendChild(el);
+                });
+                
+                page++;
+            }).catch(e=>{
+                alert('Something went wrong');
+            });
+        }
+
+        list();
+
+        showMoreBtn.onclick = (e)=>{
+            e.preventDefault();
+            list();
+        }
+
+
     </script>
 @endsection
