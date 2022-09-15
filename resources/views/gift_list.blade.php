d74102cf80a6fc36e055b8ba97fbabf58295551e
jplataquin <jp.lataquin@gmail.com>
2022-09-15 22:26:33 +0800

    test

--- resources/views/gift_list.blade.php
+++ resources/views/gift_list.blade.php
@@ -71,6 +71,8 @@
                             t.p({class:'card-text'},()=>{
                                 t.txt('Status: '+item.status);
                                 t.br();
+                                t.txt('Brand: '+item.brand_name);
+                                t.br();
                                 t.txt('Expiry: '+item.expires_at);
                             });
                             t.a({href:'#',class:'btn btn-primary'},'View');
