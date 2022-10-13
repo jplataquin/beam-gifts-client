f1bca62fa2dd74771ca1e64a2a7b827c3d667b79
jplataquin <jp.lataquin@gmail.com>
2022-10-13 13:28:27 +0800

    test

--- resources/views/item.blade.php
+++ resources/views/item.blade.php
@@ -34,8 +34,8 @@
                     </div>
                     <div class="separator"></div>
                     <div class="brand-row d-flex align-items-center">
-                        <img src="includes/images/placeholder.png" alt="">
-                        <a href="#" class="brandname ms-2">Brand Name</a>
+                        <img src="{{config('app')['api_base_url']}}storage/photos/brand/100px/{{$brand->photo['100px']}}" alt="{{$brand->name}}">
+                        <a href="#" class="brandname ms-2">{{$brand->name}}</a>
                     </div>
                 </div>
             </div>
