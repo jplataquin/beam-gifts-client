e6aada32be960c706a414ad5c1edd447514936cd
jplataquin <jp.lataquin@gmail.com>
2022-10-16 21:53:51 +0800

    test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -104,7 +104,7 @@
                                     <div class="shopping-cart">
                                             <img class="cart-icon" src="{{ asset('images/cart.png') }}" alt="">
                                             <p id="cart-quantity" class="cart-quantity">{{ count( \Cart::getContent()) }}</p>
-                                            {{ print_r(\Cart::getContent()) }}
+                                            {{ print_r(\Cart::getContent()['items']) }}
                                     </div>
                                 </a>
                             </li>
