2423aecce20fd5cd7c343baa694252fdd6f1146a
jplataquin <jp.lataquin@gmail.com>
2022-10-17 16:03:58 +0800

    test

--- resources/views/brands.blade.php
+++ resources/views/brands.blade.php
@@ -17,23 +17,15 @@
                 <div class="col-lg-2 col-md-3 filter-column">
                     <ul class="nav nav-pills fltCont mb-3" id="tab" role="tablist">
                         <li class="nav-item" role="presentation">
-                          <button class="fltItem active" id="filter-all-tab" data-bs-toggle="pill" data-bs-target="#filter-all" type="button" role="tab" aria-controls="filter-all" aria-selected="true">All</button>
+                          <button class="fltItem active" data-bs-toggle="pill" data-value="" type="button" role="tab" aria-controls="filter-all" aria-selected="true">All</button>
                         </li>
-                        <li class="nav-item" role="presentation">
-                          <button class="fltItem" id="filter-drinks-tab" data-bs-toggle="pill" data-bs-target="#filter-drinks" type="button" role="tab" aria-controls="filter-drinks" aria-selected="true">Brand Type 1</button>
-                        </li>
-                        <li class="nav-item" role="presentation">
-                          <button class="fltItem" id="filter-ltNight-tab" data-bs-toggle="pill" data-bs-target="#filter-ltNight" type="button" role="tab" aria-controls="filter-ltNight" aria-selected="false">Brand Type 2</button>
-                        </li>
-                        <li class="nav-item" role="presentation">
-                          <button class="fltItem" id="filter-desserts-tab" data-bs-toggle="pill" data-bs-target="#filter-desserts" type="button" role="tab" aria-controls="filter-desserts" aria-selected="false">Brand Type 3</button>
-                        </li>
-                        <li class="nav-item" role="presentation">
-                            <button class="fltItem" id="filter-shopping-tab" data-bs-toggle="pill" data-bs-target="#filter-shopping" type="button" role="tab" aria-controls="filter-shopping" aria-selected="false">Brand Type 4</button>
-                        </li>
-                        <li class="nav-item" role="presentation">
-                            <button class="fltItem" id="filter-delivery-tab" data-bs-toggle="pill" data-bs-target="#filter-delivery" type="button" role="tab" aria-controls="filter-delivery" aria-selected="false">Brand Type 5</button>
-                        </li>
+
+
+                        @foreach(config('categories')['options'] as $val=>$txt)
+                            <li class="nav-item" role="presentation">
+                                <button class="fltItem" data-bs-toggle="pill" data-value="{{$val}}" type="button" role="tab" aria-controls="filter-{{$val}}" aria-selected="true">{{$txt}}</button>
+                            </li>
+                        @endforeach
                     </ul>
                 </div>
                 <div class="col-lg-10 col-md-9">
@@ -43,190 +35,6 @@
                                 <div class="col-12">
                                     <h2 class="brandtype">All</h2>
                                     <!-- <p class="totals">Total 15</p> -->
-                                </div>
-                            </div>
-
-                        </div>
-                        <div class="tab-pane fade" id="filter-drinks" role="tabpanel" aria-labelledby="filter-drinks-tab" tabindex="0">
-                            <div class="row">
-                                <div class="col-12">
-                                    <h2 class="brandtype">Brand Type 1</h2>
-                                    <p class="totals">Total 3</p>
-                                </div>
-                                <div class="col-lg-4 col-md-6 col-12 my-2">
-                                    <div class="popBrandsbox">
-                                        <img src="includes/images/placeholder.png" alt="" class="popBrandbigImg">
-                                        <div class="popbrandinfo">
-                                            <img src="includes/images/placeholder.png" alt="" class="popbrandimg">
-                                            <h3 class="popbrandT">Brand 1</h3>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-4 col-md-6 col-12 my-2">
-                                    <div class="popBrandsbox">
-                                        <img src="includes/images/placeholder.png" alt="" class="popBrandbigImg">
-                                        <div class="popbrandinfo">
-                                            <img src="includes/images/placeholder.png" alt="" class="popbrandimg">
-                                            <h3 class="popbrandT">Brand 2</h3>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-4 col-md-6 col-12 my-2">
-                                    <div class="popBrandsbox">
-                                        <img src="includes/images/placeholder.png" alt="" class="popBrandbigImg">
-                                        <div class="popbrandinfo">
-                                            <img src="includes/images/placeholder.png" alt="" class="popbrandimg">
-                                            <h3 class="popbrandT">Brand 3</h3>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-                        <div class="tab-pane fade" id="filter-ltNight" role="tabpanel" aria-labelledby="filter-ltNight-tab" tabindex="0">
-                            
-                            <div class="row">
-                                <div class="col-12">
-                                    <h2 class="brandtype">Brand Type 2</h2>
-                                    <p class="totals">Total 3</p>
-                                </div>
-                                <div class="col-lg-4 col-md-6 col-12 my-2">
-                                    <div class="popBrandsbox">
-                                        <img src="includes/images/placeholder.png" alt="" class="popBrandbigImg">
-                                        <div class="popbrandinfo">
-                                            <img src="includes/images/placeholder.png" alt="" class="popbrandimg">
-                                            <h3 class="popbrandT">Brand 4</h3>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-4 col-md-6 col-12 my-2">
-                                    <div class="popBrandsbox">
-                                        <img src="includes/images/placeholder.png" alt="" class="popBrandbigImg">
-                                        <div class="popbrandinfo">
-                                            <img src="includes/images/placeholder.png" alt="" class="popbrandimg">
-                                            <h3 class="popbrandT">Brand 5</h3>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-4 col-md-6 col-12 my-2">
-                                    <div class="popBrandsbox">
-                                        <img src="includes/images/placeholder.png" alt="" class="popBrandbigImg">
-                                        <div class="popbrandinfo">
-                                            <img src="includes/images/placeholder.png" alt="" class="popbrandimg">
-                                            <h3 class="popbrandT">Brand 6</h3>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-                        <div class="tab-pane fade" id="filter-desserts" role="tabpanel" aria-labelledby="filter-desserts-tab" tabindex="0">
-                            
-                            <div class="row">
-                                <div class="col-12">
-                                    <h2 class="brandtype">Brand Type 3</h2>
-                                    <p class="totals">Total 3</p>
-                                </div>
-                                <div class="col-lg-4 col-md-6 col-12 my-2">
-                                    <div class="popBrandsbox">
-                                        <img src="includes/images/placeholder.png" alt="" class="popBrandbigImg">
-                                        <div class="popbrandinfo">
-                                            <img src="includes/images/placeholder.png" alt="" class="popbrandimg">
-                                            <h3 class="popbrandT">Brand 7</h3>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-4 col-md-6 col-12 my-2">
-                                    <div class="popBrandsbox">
-                                        <img src="includes/images/placeholder.png" alt="" class="popBrandbigImg">
-                                        <div class="popbrandinfo">
-                                            <img src="includes/images/placeholder.png" alt="" class="popbrandimg">
-                                            <h3 class="popbrandT">Brand 8</h3>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-4 col-md-6 col-12 my-2">
-                                    <div class="popBrandsbox">
-                                        <img src="includes/images/placeholder.png" alt="" class="popBrandbigImg">
-                                        <div class="popbrandinfo">
-                                            <img src="includes/images/placeholder.png" alt="" class="popbrandimg">
-                                            <h3 class="popbrandT">Brand 9</h3>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-                        <div class="tab-pane fade" id="filter-shopping" role="tabpanel" aria-labelledby="filter-shopping-tab" tabindex="0">
-                            
-                            <div class="row">
-                                <div class="col-12">
-                                    <h2 class="brandtype">Brand Type 4</h2>
-                                    <p class="totals">Total 3</p>
-                                </div>
-                                <div class="col-lg-4 col-md-6 col-12 my-2">
-                                    <div class="popBrandsbox">
-                                        <img src="includes/images/placeholder.png" alt="" class="popBrandbigImg">
-                                        <div class="popbrandinfo">
-                                            <img src="includes/images/placeholder.png" alt="" class="popbrandimg">
-                                            <h3 class="popbrandT">Brand 10</h3>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-4 col-md-6 col-12 my-2">
-                                    <div class="popBrandsbox">
-                                        <img src="includes/images/placeholder.png" alt="" class="popBrandbigImg">
-                                        <div class="popbrandinfo">
-                                            <img src="includes/images/placeholder.png" alt="" class="popbrandimg">
-                                            <h3 class="popbrandT">Brand 11</h3>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-4 col-md-6 col-12 my-2">
-                                    <div class="popBrandsbox">
-                                        <img src="includes/images/placeholder.png" alt="" class="popBrandbigImg">
-                                        <div class="popbrandinfo">
-                                            <img src="includes/images/placeholder.png" alt="" class="popbrandimg">
-                                            <h3 class="popbrandT">Brand 12</h3>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-                        <div class="tab-pane fade" id="filter-delivery" role="tabpanel" aria-labelledby="filter-delivery-tab" tabindex="0">
-                            
-                            <div class="row">
-                                <div class="col-12">
-                                    <h2 class="brandtype">Brand Type 5</h2>
-                                    <p class="totals">Total 3</p>
-                                </div>
-                                <div class="col-lg-4 col-md-6 col-12 my-2">
-                                    <div class="popBrandsbox">
-                                        <img src="includes/images/placeholder.png" alt="" class="popBrandbigImg">
-                                        <div class="popbrandinfo">
-                                            <img src="includes/images/placeholder.png" alt="" class="popbrandimg">
-                                            <h3 class="popbrandT">Brand 13</h3>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-4 col-md-6 col-12 my-2">
-                                    <div class="popBrandsbox">
-                                        <img src="includes/images/placeholder.png" alt="" class="popBrandbigImg">
-                                        <div class="popbrandinfo">
-                                            <img src="includes/images/placeholder.png" alt="" class="popbrandimg">
-                                            <h3 class="popbrandT">Brand 14</h3>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-4 col-md-6 col-12 my-2">
-                                    <div class="popBrandsbox">
-                                        <img src="includes/images/placeholder.png" alt="" class="popBrandbigImg">
-                                        <div class="popbrandinfo">
-                                            <img src="includes/images/placeholder.png" alt="" class="popbrandimg">
-                                            <h3 class="popbrandT">Brand 15</h3>
-                                        </div>
-                                    </div>
                                 </div>
                             </div>
 
@@ -247,11 +55,23 @@
     const imgBaseUrl = '{{config("app")["api_base_url"]}}';
     const t = new Template();
 
-    let page = 1;
+    let page        = 1;
+    let category    = '';
+
+
+    Array.from(document.querySelectorAll('.fltItm')).map(filter => {
+
+        filter.onclick = (e)=>{
+            page = 1;
+            category = filter.getAttribute('data-value');
+            getList();
+        }
+    });
 
     function getList(){
         window.util.$get('/api/brand_list',{
-            'page' : page
+            'page'      : page,
+            'category:' : category 
         }).then(reply => {
             
             if(!reply.status){
