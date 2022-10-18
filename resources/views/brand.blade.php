44b384ba48f1c39acb90c03ff5d2efe11a44546a
jplataquin <jp.lataquin@gmail.com>
2022-10-19 02:40:11 +0800

    test

--- resources/views/brand.blade.php
+++ resources/views/brand.blade.php
@@ -9,32 +9,8 @@
         </div>
         <div class="col-md-6 mt-3 mt-md-0">
             <h5 class="product-title">{{ $brand->name }}</h5>
-            <p class="product-shortDESC brandDescription">{{ $brand->description }}
-                asdasdasdasdsadasdajsdijasodjaoidjoiasjdad
-                iaijdsoiasjdoijasodijasd
-                oaiojsdoajoidasjdjas
-                oiajodsijaidojasiodjasoidjasoijdaoisdjoaijdasd
-                oijiadiojaidjaidjisoadjiasojdsiajdajsdaojsdoajdiasojdioasjdas
-                aoijsdoiajdoiasjdioajsidojaiodjiasodjiajdasoidjaiodjioadjiasdj
-                oiaijidojasiojaiodjiajdiajdiasjdiasjidjsaidjioadjosijdioasjaoijsd
-                oiajsdoiajsoidjasiodjasiodjaoidjaoidjasiojdasiodjoiajdaisdjadioasjiodjasd
-                oiajsdoiajdoiajidjasiodjasiodjaoisdjisajdioasdjasiodjasdjads
-                
-                oijiadiojaidjaidjisoadjiasojdsiajdajsdaojsdoajdiasojdioasjdas
-                aoijsdoiajdoiasjdioajsidojaiodjiasodjiajdasoidjaiodjioadjiasdj
-                oiaijidojasiojaiodjiajdiajdiasjdiasjidjsaidjioadjosijdioasjaoijsd
-                oiajsdoiajsoidjasiodjasiodjaoidjaoidjasiojdasiodjoiajdaisdjadioasjiodjasd
-                oiajsdoiajdoiajidjasiodjasiodjaoisdjisajdioasdjasiodjasdjads
-                oijiadiojaidjaidjisoadjiasojdsiajdajsdaojsdoajdiasojdioasjdas
-                aoijsdoiajdoiasjdioajsidojaiodjiasodjiajdasoidjaiodjioadjiasdj
-                oiaijidojasiojaiodjiajdiajdiasjdiasjidjsaidjioadjosijdioasjaoijsd
-                oiajsdoiajsoidjasiodjasiodjaoidjaoidjasiojdasiodjoiajdaisdjadioasjiodjasd
-                oiajsdoiajdoiajidjasiodjasiodjaoisdjisajdioasdjasiodjasdjads
-                oijiadiojaidjaidjisoadjiasojdsiajdajsdaojsdoajdiasojdioasjdas
-                aoijsdoiajdoiasjdioajsidojaiodjiasodjiajdasoidjaiodjioadjiasdj
-                oiaijidojasiojaiodjiajdiajdiasjdiasjidjsaidjioadjosijdioasjaoijsd
-                oiajsdoiajsoidjasiodjasiodjaoidjaoidjasiojdasiodjoiajdaisdjadioasjiodjasd
-                oiajsdoiajdoiajidjasiodjasiodjaoisdjisajdioasdjasiodjasdjads
+            <p class="product-shortDESC brandDescription">
+                {{ $brand->description }}
             </p>
             
             <div class="branchList">
@@ -45,35 +21,71 @@
                         <h6>{{$branch['phone']}}</h6>
                     </div>
                 @endforeach
-                @foreach($brand->branches as $branch)
-                    <div class="border border-primary p-1 mb-2">
-                        <h5>{{$branch['name']}}</h5>
-                        <label>{{$branch['address']}}</label>
-                        <h6>{{$branch['phone']}}</h6>
-                    </div>
-                @endforeach
-                @foreach($brand->branches as $branch)
-                    <div class="border border-primary p-1 mb-2">
-                        <h5>{{$branch['name']}}</h5>
-                        <label>{{$branch['address']}}</label>
-                        <h6>{{$branch['phone']}}</h6>
-                    </div>
-                @endforeach
             </div>
         </div>
     </div>
-    <div class="row">
-        <div class="col">
-            @foreach($items as $item)
+    <div class="row mt-3 mb-3">
+        @foreach($items as $item)
+            <div class="col">
+          
                 <div class="card" style="width: 200px;">
                     <img src="{{config('app')['api_base_url'].'storage/photos/item/200px/'.$item->photo['200px']}}" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">{{$item->name}}</h5>
-                        <a href="/item/{{str_replace(' ','-',$brand->name)}}/{{str_replace(' ','-',$item->name)}}" class="btn btn-primary">Buy Gift</a>
+                        <a href="/item/{{str_replace(' ','-',$brand->name)}}/{{str_replace(' ','-',$item->name)}}" class="btn btn-primary">View</a>
                     </div>
                 </div>
-            @endforeach
-        </div>
+            
+            </div>
+
+            <div class="col">
+          
+           <div class="card" style="width: 200px;">
+                    <img src="{{config('app')['api_base_url'].'storage/photos/item/200px/'.$item->photo['200px']}}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">{{$item->name}}</h5>
+                        <a href="/item/{{str_replace(' ','-',$brand->name)}}/{{str_replace(' ','-',$item->name)}}" class="btn btn-primary">View</a>
+                    </div>
+                </div>
+            
+            </div>
+
+
+            <div class="card" style="width: 200px;">
+                    <img src="{{config('app')['api_base_url'].'storage/photos/item/200px/'.$item->photo['200px']}}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">{{$item->name}}</h5>
+                        <a href="/item/{{str_replace(' ','-',$brand->name)}}/{{str_replace(' ','-',$item->name)}}" class="btn btn-primary">View</a>
+                    </div>
+                </div>
+            
+            </div>
+
+
+
+            <div class="card" style="width: 200px;">
+                    <img src="{{config('app')['api_base_url'].'storage/photos/item/200px/'.$item->photo['200px']}}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">{{$item->name}}</h5>
+                        <a href="/item/{{str_replace(' ','-',$brand->name)}}/{{str_replace(' ','-',$item->name)}}" class="btn btn-primary">View</a>
+                    </div>
+                </div>
+            
+            </div>
+
+
+
+            <div class="card" style="width: 200px;">
+                    <img src="{{config('app')['api_base_url'].'storage/photos/item/200px/'.$item->photo['200px']}}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">{{$item->name}}</h5>
+                        <a href="/item/{{str_replace(' ','-',$brand->name)}}/{{str_replace(' ','-',$item->name)}}" class="btn btn-primary">View</a>
+                    </div>
+                </div>
+            
+            </div>
+
+        @endforeach
     </div>
 </div>
 
