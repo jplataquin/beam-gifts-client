00bc0fe88ab86cb1fa138fb2aed92872947a7a9b
jplataquin <jp.lataquin@gmail.com>
2022-10-24 16:03:02 +0800

    test

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facad\Validator;
+use Illuminate\Support\Facades\Validator;
 
 class AppServiceProvider extends ServiceProvider
 {
