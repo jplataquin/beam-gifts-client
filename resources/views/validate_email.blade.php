a215733c95cbfdacc6a906a73bdfaeb7381df91b
jplataquin <jp.lataquin@gmail.com>
2022-10-24 18:33:13 +0800

    test

--- resources/views/validate_email.blade.php
+++ resources/views/validate_email.blade.php
@@ -5,17 +5,17 @@
 <div class="container pb-5">
     <div class="text-center">
         <h3>To contiue with all our features and services please validate ownership of your email</h3>
-        <h5>We sent validation link to your E-mail</h5>
+        <h5>We sent a validation link to your E-mail</h5>
     </div>
 
     <div class="row">
         <div class="col form-group">
-            <label>Email</label>
+            <label>&nbsp;</label>
             <input type="email" class="form-control" value="{{$email}}"/>
         </div>
     </div>
     <div class="row">
-        <div class="col form-group text-end">
+        <div class="col form-group text-center">
             <label>Did not receive any email?</label>
             <br>
             <button id="resend" class="btn btn-primary">Resend</button>
