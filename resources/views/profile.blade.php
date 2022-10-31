e5d37839f9dbfbd5d881e4a193c9e71ee5767680
jplataquin <jp.lataquin@gmail.com>
2022-10-31 16:56:58 +0800

    test

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -7,18 +7,18 @@
 
     <div class="row">
         <div class="col-12">
-            <div class="form-group mb-5">
+            <div class="form-group mb-3">
                 <label>Firstname</label>
                 <input type="text" class="form-control" readonly="true" value="{{$firstname}}"/>
             </div>
         </div>
-        <div class="col-12">
+        <div class="col-12 mb-3">
             <div class="form-group">
                 <label>Lastname</label>
                 <input type="text" class="form-control" readonly="true" value="{{$lastname}}"/>
             </div>
         </div>
-        <div class="col-12">
+        <div class="col-12 mb-3">
             <div class="form-group">
                 <label>Email</label>
                 <input type="text" class="form-control" readonly="true" value="{{$email}}"/>
