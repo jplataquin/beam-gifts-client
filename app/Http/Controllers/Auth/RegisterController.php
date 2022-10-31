a4911fa44462de8695928f77a44f2e25747b49d3
jplataquin <jp.lataquin@gmail.com>
2022-10-31 21:27:10 +0800

    test

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -53,7 +53,7 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'bithday' => ['required', 'string', 'date_format:YYYY-MM-DD'],
+            'birthday' => ['required', 'string', 'date_format:YYYY-MM-DD'],
             'firstname' => ['required', 'string', 'max:255'],
             'lastname' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
