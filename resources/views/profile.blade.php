a6b3eac08683e544f29d8230e963be18d2ccdb61
jplataquin <jp.lataquin@gmail.com>
2022-10-31 22:24:00 +0800

    test

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -5,20 +5,27 @@
 <div class="container">
     <h1>Profile</h1>
 
-    <div class="row">
-        <div class="col-12">
-            <div class="form-group mb-3">
+    <div class="row justify-content-center">
+    <div class="col-8 mb-3">
+            <div class="form-group">
+                <label>Birthday</label>
+                <input type="text" class="form-control" readonly="true" value="{{$birthday}}"/>
+            </div>
+        </div>
+
+        <div class="col-8 mb-3">
+            <div class="form-group">
                 <label>Firstname</label>
                 <input type="text" class="form-control" readonly="true" value="{{$firstname}}"/>
             </div>
         </div>
-        <div class="col-12 mb-3">
+        <div class="col-8 mb-3">
             <div class="form-group">
                 <label>Lastname</label>
                 <input type="text" class="form-control" readonly="true" value="{{$lastname}}"/>
             </div>
         </div>
-        <div class="col-12 mb-3">
+        <div class="col-8 mb-3">
             <div class="form-group">
                 <label>Email</label>
                 <input type="text" class="form-control" readonly="true" value="{{$email}}"/>
