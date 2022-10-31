d50942045382924bfeb844c2e5fd2e67a9d37c4f
jplataquin <jp.lataquin@gmail.com>
2022-10-31 16:53:16 +0800

    test

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -2,5 +2,28 @@
 
 @section('content')
 
-<h1>Hello World</h1>
+<div class="container">
+    <h1>Profile</h1>
+    
+    <div class="row">
+        <div class="col-12">
+            <div class="form-group">
+                <label>Firstname</label>
+                <input type="text" readonly="true" value="{{firstname}}"/>
+            </div>
+        </div>
+        <div class="col-12">
+            <div class="form-group">
+                <label>Lastname</label>
+                <input type="text" readonly="true" value="{{lastname}}"/>
+            </div>
+        </div>
+        <div class="col-12">
+            <div class="form-group">
+                <label>Email</label>
+                <input type="text" readonly="true" value="{{email}}"/>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
