b9990782e7739a50edf08eac3925186536113b62
jplataquin <jp.lataquin@gmail.com>
2022-11-02 15:06:57 +0800

    test

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -7,31 +7,33 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Profile') }}</div>
+                <div class="card-header">
+                    <h3>{{ __('Profile') }}</h3>
+                </div>
 
                 <div class="card-body">
 
                       <div class="row">
-                        <div class="col-8 mb-3">
+                        <div class="col-12 mb-3">
                             <div class="form-group">
                                 <label>Birthday</label>
                                 <input type="text" class="form-control" readonly="true" value="{{$birthday}}"/>
                             </div>
                         </div>
 
-                        <div class="col-8 mb-3">
+                        <div class="col-12 mb-3">
                             <div class="form-group">
                                 <label>Firstname</label>
                                 <input type="text" class="form-control" readonly="true" value="{{$firstname}}"/>
                             </div>
                         </div>
-                        <div class="col-8 mb-3">
+                        <div class="col-12 mb-3">
                             <div class="form-group">
                                 <label>Lastname</label>
                                 <input type="text" class="form-control" readonly="true" value="{{$lastname}}"/>
                             </div>
                         </div>
-                        <div class="col-8 mb-3">
+                        <div class="col-12 mb-3">
                             <div class="form-group">
                                 <label>Email</label>
                                 <input type="text" class="form-control" readonly="true" value="{{$email}}"/>
