2c2585489014895919b1f67de8cdab10e0b79e7f
jplataquin <jp.lataquin@gmail.com>
2022-11-02 15:07:20 +0800

    test

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -4,7 +4,7 @@
 
 <div class="container">
    
-    <div class="row justify-content-center">
+    <div class="row justify-content-center mb-5">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">
