43e8536a0bf245ce67763155b355e7769a64b013
jplataquin <jp.lataquin@gmail.com>
2022-11-02 14:40:30 +0800

    test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -181,7 +181,7 @@
                     @endif
                 @else
                     <li class="my-2">
-                       <a class="dropdown-item" href="{{ route('logout') }}"
+                       <a href="{{ route('logout') }}"
                             onclick="event.preventDefault();
                                         document.getElementById('logout-form').submit();">
                             {{ __('Logout') }}
