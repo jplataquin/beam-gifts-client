c537e2589345f77e3c2bac240a269769f7319b84
jplataquin <jp.lataquin@gmail.com>
2022-11-02 15:05:53 +0800

    test

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -5,36 +5,44 @@
 <div class="container">
    
     <div class="row justify-content-center">
-        <div class="col-8 mb-3">
-            <h1>Profile</h1>
-        </div>
-    </div>
-    <div class="row justify-content-center">
-        <div class="col-8 mb-3">
-            <div class="form-group">
-                <label>Birthday</label>
-                <input type="text" class="form-control" readonly="true" value="{{$birthday}}"/>
-            </div>
-        </div>
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Profile') }}</div>
 
-        <div class="col-8 mb-3">
-            <div class="form-group">
-                <label>Firstname</label>
-                <input type="text" class="form-control" readonly="true" value="{{$firstname}}"/>
-            </div>
-        </div>
-        <div class="col-8 mb-3">
-            <div class="form-group">
-                <label>Lastname</label>
-                <input type="text" class="form-control" readonly="true" value="{{$lastname}}"/>
-            </div>
-        </div>
-        <div class="col-8 mb-3">
-            <div class="form-group">
-                <label>Email</label>
-                <input type="text" class="form-control" readonly="true" value="{{$email}}"/>
+                <div class="card-body">
+
+                      <div class="row">
+                        <div class="col-8 mb-3">
+                            <div class="form-group">
+                                <label>Birthday</label>
+                                <input type="text" class="form-control" readonly="true" value="{{$birthday}}"/>
+                            </div>
+                        </div>
+
+                        <div class="col-8 mb-3">
+                            <div class="form-group">
+                                <label>Firstname</label>
+                                <input type="text" class="form-control" readonly="true" value="{{$firstname}}"/>
+                            </div>
+                        </div>
+                        <div class="col-8 mb-3">
+                            <div class="form-group">
+                                <label>Lastname</label>
+                                <input type="text" class="form-control" readonly="true" value="{{$lastname}}"/>
+                            </div>
+                        </div>
+                        <div class="col-8 mb-3">
+                            <div class="form-group">
+                                <label>Email</label>
+                                <input type="text" class="form-control" readonly="true" value="{{$email}}"/>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </div>
     </div>
+    
 </div>
 @endsection
