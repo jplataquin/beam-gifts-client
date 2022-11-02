e1c4522fe3ade494618a9b30076d066679a4ae84
jplataquin <jp.lataquin@gmail.com>
2022-11-02 23:55:10 +0800

    test

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -6,8 +6,8 @@
 
         @foreach($items as $item)
             <div class="border border-primary mb-3 p-2">
-                <div class="row">
-                    <div class="col-12 bg-darkmagenta">
+                <div class="row bg-darkmagenta">
+                    <div class="col-12">
                         <h3 class="fontcolor-white">{{$item->name}}</h3>
                     </div>
                 </div>
