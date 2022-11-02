f02afcc4e8214929fa18e16e5fc1410df84848af
jplataquin <jp.lataquin@gmail.com>
2022-11-02 18:23:42 +0800

    test

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+    <div class="row">
+        <div class="col-12 text-center">
             <img src="{{ asset('images/gift-1.png') }}" class="w-300" />
         </div>
     </div>
