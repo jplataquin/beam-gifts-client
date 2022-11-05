e629c0d444807c9c2b0bc126336f9838821a2e68
jplataquin <jp.lataquin@gmail.com>
2022-11-05 18:48:28 +0800

    update

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,69 @@
 import './bootstrap';
 
+
+
+/** Feeze UI **/
+/*
+
+
+
+
+#The MIT License (MIT)
+
+Copyright (c) 2017 Alex Radulescu
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+(() => {
+
+    /**
+     * Setup the freeze element to be appended
+     */
+    let freezeHtml = document.createElement('div');
+        freezeHtml.classList.add('freeze-ui');
+
+    /** 
+    * Freezes the UI
+    * options = { 
+    *   selector: '.class-name' -> Choose an element where to limit the freeze or leave empty to freeze the whole body. Make sure the element has position relative or absolute,
+    *   text: 'Magic is happening' -> Choose any text to show or use the default "Loading". Be careful for long text as it will break the design.
+    * }
+    */
+    window.FreezeUI = (options = {}) => {
+        let parent = document.querySelector(options.selector) || document.body;
+        freezeHtml.setAttribute('data-text', options.text || 'Loading');
+        if (document.querySelector(options.selector)) {
+            freezeHtml.style.position = 'absolute';
+        }
+        parent.appendChild(freezeHtml);
+    };
+    
+    /**
+     * Unfreezes the UI.
+     * No options here.
+     */
+    window.UnFreezeUI = () => {
+        let element = document.querySelector('.freeze-ui');
+        if (element) {
+ 
+           element.classList.add('is-unfreezing');
+            setTimeout(() => {
+                if (element) {
+                    element.classList.remove('is-unfreezing');
+                    element.parentElement.removeChild(element);
+                }
+            }, 250);
+        }
+    };
+
+})();
+
+
 window.util = {};
 
 window.util.spaceToDash = (text) =>{
