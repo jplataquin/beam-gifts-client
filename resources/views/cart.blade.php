8bdb2671cd46d10d91676573818b74587373cac3
jplataquin <jp.lataquin@gmail.com>
2022-11-06 15:56:52 +0800

    update

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -36,38 +36,6 @@
                     </div>
                 </div>
             @endforeach
-
-            @foreach($items as $item)
-                <div class="mb-3">
-                    <div class="row bg-darkmagenta mb-2">
-                        <div class="col-12 pt-2">
-                            <h5 class="fontcolor-white">{{$item->name}}</h5>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-lg-2 mb-3">
-                            <img class="img" src="{{config('app')['api_base_url']}}storage/photos/item/150px/{{$item->attributes['image']}}"/>
-                        </div>
-                        <div class="col-lg-10">
-                            <div class="row">
-                                <div class="col-7">
-                                    
-                                    <div>
-                                        Qty: {{$item->quantity}}
-                                    </div>
-                                    <div>
-                                        Price: {{number_format($item->price,2)}}
-                                    </div>
-                                </div>
-                                <div class="col-5 text-end">
-                                    <h3>PHP {{number_format($item->quantity * $item->price,2)}}</h3>
-                                    <button data-id="{{$item->id}}" class="removeBtn btn btn-danger">Remove</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            @endforeach
         </div>
         
         <div class="row">
@@ -121,7 +89,6 @@
                 let uid     = reply.data.uid;
                 let method  = reply.data.method;
                 
-                console.log(reply);
 
                 switch(method){
                     case 'cc':
