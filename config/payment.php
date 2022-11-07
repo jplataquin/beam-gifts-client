2d5e41aff2c7119207eacf635466607be33b488b
jplataquin <jp.lataquin@gmail.com>
2022-11-07 11:13:25 +0800

    test

--- config/payment.php
+++ config/payment.php
@@ -5,11 +5,13 @@ return [
     'service_fee' => 100,
     
     'payment_processor_fee' => [
+        
         'cc' => function($amount) {
             return ($amount * 0.05) + 15;
         },
+
         'gc' => function($amount){
             return 0;
         }
     ],
-]
+];
