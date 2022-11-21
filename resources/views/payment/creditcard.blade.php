4419d727da1b244e7423d58426c81f9f2803f155
jplataquin <jp.lataquin@gmail.com>
2022-11-22 01:02:47 +0800

    stream line

--- resources/views/payment/creditcard.blade.php
+++ resources/views/payment/creditcard.blade.php
@@ -381,6 +381,15 @@
         return (checksum % 10) == 0;
     }
 
+
+    function validateEmail(email){
+        return String(email)
+            .toLowerCase()
+            .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            );
+    }
+
     function validate(){
 
         //Remove all invalid classes
@@ -400,6 +409,11 @@
             flag = false;
         }
 
+
+        if(!validateEmail(email.value)){
+            email.classList.add('input-field-invalid');
+            flag = false;
+        }
 
         $q('.v-required').apply((el)=>{
 
