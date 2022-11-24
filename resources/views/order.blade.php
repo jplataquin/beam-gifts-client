cf084aa7ebbf982e2357ae15ff496163f66c7251
jplataquin <jp.lataquin@gmail.com>
2022-11-24 19:56:57 +0800

    fix

--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -6,7 +6,7 @@
         <div class="row">
             <div class="col-12">
 
-                <div class="card" style="width: 18rem;">
+                <div class="card">
                     <div class="card-header">
                         Order Ref: {{$order->uid}}
                     </div>
