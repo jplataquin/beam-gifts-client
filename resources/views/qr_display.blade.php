4b3231808454ecd7321267cd4d4e3c68d62ec6ec
jplataquin <jp.lataquin@gmail.com>
2022-11-25 17:37:38 +0800

    test

--- resources/views/qr_display.blade.php
+++ resources/views/qr_display.blade.php
@@ -19,16 +19,18 @@
                 <h2 class="mt-3 mb-3">{{$item->item_name}}</h2>
             </div>
         </div>
-        <div class="col-md-6 text-center">
-            <div class="d-flex align-items-center">
-                <div class="box w-100">
-                    <div class="text-center mb-3" width="100%" id="qr_logo"></div>
-                    
-                    <div class="border border-primary mb-3"> 
-                        <h2 class="mt-3 mb-3">Used: {{$item->consumed}} / {{$item->quantity}}</h2>
-                    </div>
+        <div class="col-md-6 d-flex align-items-center">
+         
+            <div class="box w-100">
+                <div class="text-center mb-3" width="100%" id="qr_logo"></div>
+                
+                <!--
+                <div class="border border-primary mb-3"> 
+                    <h2 class="mt-3 mb-3">Used: {{$item->consumed}} / {{$item->quantity}}</h2>
                 </div>
+                -->
             </div>
+        
         </div>
         
     </div>
