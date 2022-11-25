d261fe64827894d2870c3d5c893a872673acf065
jplataquin <jp.lataquin@gmail.com>
2022-11-25 13:17:53 +0800

    test

--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -11,7 +11,7 @@
                         <strong>Order Ref:</strong> {{ str_pad($order->id,6,0,STR_PAD_LEFT)}}
                     </div>
                     <div class="card-body">
-                        <h5 class="card-title">Status: <span style="font-color:green">{{$status}}</span> </h5>
+                        <h5 class="card-title">Status: <span style="color:green">{{$status}}</span> </h5>
                         <p>
                             <strong>Total:</strong> PHP {{number_format($order->amount,2)}}</p>
                         <p>
