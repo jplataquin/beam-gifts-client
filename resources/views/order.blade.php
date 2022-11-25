8d7dde0973f7674035a58c1b0cf34d97ca0292dd
jplataquin <jp.lataquin@gmail.com>
2022-11-25 13:07:56 +0800

    test

--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -43,9 +43,9 @@
                                     
                                 </div>
                                 <div class="col-lg-5 col pt-2 text-center">
-                                    <button class="btn btn-warning mb-2">Copy Link</button>
-                                    &nbsp;
-                                    <button class="btn btn-primary mb-2">Open Link</button>
+                                    <button class="btn btn-warning mb-2" width="50%">Copy Link</button>
+                                    
+                                    <button class="btn btn-primary mb-2" width="50%">Open Link</button>
                                 </div>
                             </div>
                     
