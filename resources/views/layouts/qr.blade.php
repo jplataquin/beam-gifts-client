b144af7a91439d27092eab96a2dd057b7cd5f40a
jplataquin <jp.lataquin@gmail.com>
2022-11-25 21:07:34 +0800

    test

--- resources/views/layouts/qr.blade.php
+++ resources/views/layouts/qr.blade.php
@@ -4,6 +4,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!--
+    <meta property="og:title" content="{{$item->item_name}}" />
+    <meta property="og:image" content="https://ia.media-imdb.com/images/rock.jpg" />
+    <meta name="description" content="You received an E-gift">
+-->
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
@@ -16,7 +21,8 @@
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     
-
+    
+    <!-- {{config('app')['api_base_url']}}storage/photos/item/400px/{{$photo['400px']}} -->
 </head>
 <body>
     <div id="app">
