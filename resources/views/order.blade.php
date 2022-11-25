4f9f2c5208293930aa4429baf83c444b1122fe08
jplataquin <jp.lataquin@gmail.com>
2022-11-25 13:12:02 +0800

    test

--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -42,8 +42,8 @@
                                     {{$item->price}}
                                     
                                 </div>
-                                <div class="col-lg-5 col text-center">
-                                    <div class="d-grid gap-2 col-6 mx-auto">
+                                <div class="col-lg-5 col pt-2 text-center">
+                                    <div class="d-grid gap-2 mx-auto">
                                         <button class="btn btn-warning mb-2">Copy Link</button>
                                         <button class="btn btn-primary mb-2">Open Link</button>
                                     </div>
