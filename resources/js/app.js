730bdece5361ccc472eeb2cd1d77ccf50929e8e0
jplataquin <jp.lataquin@gmail.com>
2022-11-28 16:52:40 +0800

    test

--- resources/js/app.js
+++ resources/js/app.js
@@ -221,7 +221,7 @@ window.toastCenter = (text)=>{
         position: "center", // `left`, `center` or `right`
         stopOnFocus: true, // Prevents dismissing of toast on hover
         style: {
-          background: "linear-gradient(to right, #00b09b, #96c93d)",
+          background: "linear-gradient(to right, #C70A80, #590696)",
         },
     }).showToast();
 }
