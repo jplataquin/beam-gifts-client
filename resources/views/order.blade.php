e4035440088a78a063a20caa7192dd37ecfc2444
jplataquin <jp.lataquin@gmail.com>
2022-11-28 15:52:05 +0800

    test

--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -17,7 +17,7 @@
 
                                 @foreach($calculation->order as $key)
                                     <p>
-                                        <strong>{{ str_replace("_", " ",$key) }} :</strong> PHP {{number_format($calculation->$key,2)}}
+                                        <strong>{{  ucwords(str_replace("_", " ",$key)) }} :</strong> PHP {{number_format($calculation->$key,2)}}
                                     </p>
                                 @endforeach
                                 <p>
