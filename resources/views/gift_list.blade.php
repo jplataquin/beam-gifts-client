e93358ec1240877e2ae2b05e6ae14ba2dde440eb
jplataquin <jp.lataquin@gmail.com>
2022-12-01 22:31:44 +0800

    test

--- resources/views/gift_list.blade.php
+++ resources/views/gift_list.blade.php
@@ -108,7 +108,7 @@
                                     date: exp_arr[2]
                                 });
                                 console.log(d);
-                                 t.txt('Expiry: '+d.month().short+' '+d.dd()+', '+d.yyyy());
+                                 t.txt('Expiry: '+d.month().short+' ');
                             });
 
                             t.div({class:'text-center'},()=>{
