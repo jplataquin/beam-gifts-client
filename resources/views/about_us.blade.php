c87ddf212650e9bf23344e85066214242e9c5c88
jplataquin <jp.lataquin@gmail.com>
2023-01-26 20:18:33 +0800

    test

--- resources/views/about_us.blade.php
+++ resources/views/about_us.blade.php
@@ -4,6 +4,12 @@
 
 <div class="container">
     <h1>About Us</h1>
+    <hr>
+    <p>Beam Gifts is online gift buying and sending platform that was established to provide covenience for both the giver and receiver.</p>
+    
+    <p>Our team continously curate various products and services and insure they become the perfect gifts for all your love ones.</p>
+    <p>We work with reliable partners and providers to deliver a wide range of high quality gifts that are categorized and accessible anytime.</p>
+    
 </div>
 
 @endsection
