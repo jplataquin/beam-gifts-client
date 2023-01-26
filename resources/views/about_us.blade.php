62c56dd5e66b3d39d9bf200cc74d620aaddcac8f
jplataquin <jp.lataquin@gmail.com>
2023-01-26 20:30:35 +0800

    test

--- resources/views/about_us.blade.php
+++ resources/views/about_us.blade.php
@@ -5,29 +5,30 @@
 <div class="container">
     <h1>About Us</h1>
     <hr>
+    <div style="padding:5px; background-color:#FFFFFF">
+        <p>Hello There!</p>
 
-    <p>Hello There!</p>
-
-    <p style="text-indent:1cm;">
-        Beam Gifts is an online gift buying and sending platform that was established to provide covenience for both the giver and receiver.
-        Our team continously curate various products and services to insure they become the perfect gifts for all your love ones.
-        We work with reliable partners and providers in order to deliver a wide range of high quality gifts that are categorized and accessible anytime.
-    </p>
-    
-
-    <p>
-        Currently our area of operation is only limited to Iloilo city, but our team is working tirelessy to bring Beam Gifts to the entire country (Philippines) and beyond.
-        We hope that you will try and avail our products and services.</p>
+        <p style="text-indent:1cm;">
+            Beam Gifts is an online gift buying and sending platform that was established to provide covenience for both the giver and receiver.
+            Our team continously curate various products and services to insure they become the perfect gifts for all your love ones.
+            We work with reliable partners and providers in order to deliver a wide range of high quality gifts that are categorized and accessible anytime.
+        </p>
         
-    <p>
-        Any feedback to improve your customer experience is very much apprieciated. (feedback@beam.gifts)
-    </p>
-    
-    <p class="mt-5">
-        Regards,
-        <br>
-        Beam Gifts Team
-    </p>
+
+        <p>
+            Currently our area of operation is only limited to Iloilo city, but our team is working tirelessy to bring Beam Gifts to the entire country (Philippines) and beyond.
+            We hope that you will try and avail our products and services.</p>
+            
+        <p>
+            Any feedback to improve your customer experience is very much apprieciated. (feedback@beam.gifts)
+        </p>
+        
+        <p class="mt-5">
+            Regards,
+            <br>
+            Beam Gifts Team
+        </p>
+    </div>
 </div>
 
 @endsection
