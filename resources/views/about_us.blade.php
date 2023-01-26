23e2f1ef8beab3597be8b4eae8b76a2961b19eaf
jplataquin <jp.lataquin@gmail.com>
2023-01-26 20:24:34 +0800

    test

--- resources/views/about_us.blade.php
+++ resources/views/about_us.blade.php
@@ -5,17 +5,20 @@
 <div class="container">
     <h1>About Us</h1>
     <hr>
-    <p>Beam Gifts is online gift buying and sending platform that was established to provide covenience for both the giver and receiver.</p>
-    
     <p>
+        Beam Gifts is online gift buying and sending platform that was established to provide covenience for both the giver and receiver.
         Our team continously curate various products and services to insure they become the perfect gifts for all your love ones.
         We work with reliable partners and providers in order to deliver a wide range of high quality gifts that are categorized and accessible anytime.
     </p>
     
 
     <p>
-        Currently our area of operation is only limited to Iloilo city.
-        But our team is working tirelessy to bring Beam Gifts to the entire country (Philippines) and beyond.
+        Currently our area of operation is only limited to Iloilo city,
+        but our team is working tirelessy to bring Beam Gifts to the entire country (Philippines) and beyond.
+    </p>
+
+    <p>
+        We hope that you will try and avail our products and services. Any feedback to improve your customer experience is very much apprieciated.
     </p>
 </div>
 
