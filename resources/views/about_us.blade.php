8ee61f471b0356ecf1e838859fe745e447717c3e
jplataquin <jp.lataquin@gmail.com>
2023-01-26 20:34:43 +0800

    test

--- resources/views/about_us.blade.php
+++ resources/views/about_us.blade.php
@@ -5,7 +5,7 @@
 <div class="container">
     <h1>About Us</h1>
     <hr>
-    <div style="padding:20px; background-color:#FFFFFF; border-radius: 25px;font-size:20px;margin-bottom:10px">
+    <div style="padding:20px; background-color:#FFFFFF; border-radius: 25px;font-size:20px;margin-bottom:10px;text-align: justify;">
         <p>Hello There!</p>
 
         <p style="text-indent:1cm;">
