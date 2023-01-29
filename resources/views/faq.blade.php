9e4563a00ca3d5ef47723304e41ebb518462d55b
jplataquin <jp.lataquin@gmail.com>
2023-01-29 17:55:58 +0800

    test

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -4,14 +4,40 @@
 
 <div class="container">
     <h1>Frequenty Asked Questions (FAQ)</h1>
-    <div class="mb-5">
-        <div class="mb-3 fw-bold">
-            Q: Lorem asdasdasdasdasdsadasdasdasdsadsad
+    
+
+    <div class="mb-5 bg-light p-3">
+        <div class="mb-3 fw-bold fs-4">
+            Q: How can the recieve claim a gift item?
         </div>    
-        <div>
-            A: asdasdasdasdasdasdasdasd
+        <div class="fs-3">
+            A: 
         </div>
     </div>
+
+
+    <div class="mb-5 bg-light p-3">
+        <div class="mb-3 fw-bold fs-4">
+            Q: What happens if I sent the gift to the wrong person?
+        </div>    
+        <div class="fs-3">
+            A:  
+        </div>
+    </div>
+    
+    
+
+    <div class="mb-5 bg-light p-3">
+        <div class="mb-3 fw-bold fs-4">
+            Q: How do I refund a gift item?
+        </div>    
+        <div class="fs-3">
+            A: Only the buyer of the gift can request for a refund. 
+            In order to initiate a refund, you can email the details of your purchase at "refund@beam.gifts".
+            Note that 
+        </div>
+    </div>
+
 
 </div>
 
