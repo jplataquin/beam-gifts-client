43c7493acfab2341d54a5fb1b27b3f5219df02b3
jplataquin <jp.lataquin@gmail.com>
2023-01-29 18:11:10 +0800

    test

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -31,8 +31,10 @@
             Q: How do I refund a gift item?
         </div>    
         <div class="fs-3">
-            A: Only the buyer can request for a refund. 
+            A: Only the buyer of the gift can request for a refund. 
+            <br>
             In order to initiate the process, please email us using your registered email at "refund@beam.gifts".
+            <br>
             Note that refunds are subject to our <a href="/tos">terms and services</a>, and due to third party payment providers we will not be able to refund
             the exact amount.
         </div>
