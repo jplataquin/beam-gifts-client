47cc7f276949ad61986eb6291a8484fbdc86c301
jplataquin <jp.lataquin@gmail.com>
2023-01-30 13:54:22 +0800

    test

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -6,7 +6,7 @@
     <h1>Frequenty Asked Questions (FAQ)</h1>
     
 
-    <div class="mb-5 bg-white p-3">
+    <div class="mb-3 bg-white p-3">
         <div class="mb-3 fw-bold fs-4">
             Q: How to claim a E-gift?
         </div>    
@@ -17,7 +17,7 @@
     </div>
 
 
-    <div class="mb-5 bg-white p-3">
+    <div class="mb-3 bg-white p-3">
         <div class="mb-3 fw-bold fs-4">
             Q: What happens if I sent the E-gift to the wrong person?
         </div>    
@@ -27,7 +27,7 @@
         </div>
     </div>
     
-    <div class="mb-5 bg-white p-3">
+    <div class="mb-3 bg-white p-3">
         <div class="mb-3 fw-bold fs-4">
             Q: Is it safe to share my E-gift QR code publicly or online?
         </div>    
@@ -38,7 +38,7 @@
     </div>
 
 
-    <div class="mb-5 bg-white p-3">
+    <div class="mb-3 bg-white p-3">
         <div class="mb-3 fw-bold fs-4">
             Q: What happens if the E-gift I want to claim is not available or is out of stock.
         </div>    
@@ -49,21 +49,21 @@
     </div>
 
 
-    <div class="mb-5 bg-white p-3">
+    <div class="mb-3 bg-white p-3">
         <div class="mb-3 fw-bold fs-4">
             Q: Can I request to change the QR code of the E-gift?
         </div>    
-        <div class="fs-2">
+        <div class="fs-5">
             A: No, Each E-gift has a unique QR code and once generated it cannot be changed.
         </div>
     </div>
     
     
-    <div class="mb-5 bg-white p-3">
+    <div class="mb-3 bg-white p-3">
         <div class="mb-3 fw-bold fs-4">
             Q: How do I refund a E-gift item?
         </div>    
-        <div class="fs-2">
+        <div class="fs-5">
             A: Only the user who bought the E-gift can request for a refund. 
             In order to initiate the process, please email us using your registered email at "refund@beam.gifts".
             Note that refunds are subject to our <a href="/tos">terms of service</a>, and due to third party payment providers we will not be able to refund
@@ -72,11 +72,11 @@
     </div>
 
 
-    <div class="mb-5 bg-white p-3">
+    <div class="mb-3 bg-white p-3">
         <div class="mb-3 fw-bold fs-4">
             Q: What happens if the brand or service is no longer available and my E-gift has not yet been claimed.
         </div>    
-        <div class="fs-3">
+        <div class="fs-5">
             A: In the event that the brand or service has closed down or no longer exists, the user who bought the E-gift can request for a refund.
             Note that the E-gift must not be expired and is subject to the conditions in our <a href="/tos">terms of service</a>,.
         </div>
