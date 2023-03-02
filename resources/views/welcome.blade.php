ace932bea44474dd65b9299f3c8181bad5fc2a63
jplataquin <jp.lataquin@gmail.com>
2023-03-02 15:57:48 +0800

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -104,7 +104,7 @@
             <div class="row mt-3 category-row">
             @foreach( config('brand_categories.options') as $key=>$text)
                 <div class="col-lg-2 col-md-4 col-6 text-center">
-                    <div class="occasion p-2 my-2" href="/weh">
+                    <div class="clickable-link occasion p-2 my-2"  data-href="/brands/{{preg_replace('/[[:space:]]+/', '-', strtolower($text));}}">
                         
                         <img class="occasionImg m-auto mb-2" src="{{ asset( 'images/icons/'.$key.'.png' ) }}" alt="">
                         {{$text}}
