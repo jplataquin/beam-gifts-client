2823a27713ce224681a4a8dcffddf0a63cd50025
jplataquin <jp.lataquin@gmail.com>
2023-03-02 15:36:41 +0800

    test

--- resources/views/gifts.blade.php
+++ resources/views/gifts.blade.php
@@ -103,7 +103,7 @@
 
            
             let filterButton = $q('.fltItem [data-value="'+option+'"]').first();
-            console.log('filterbuttoin',filterButton);
+            console.log('filterbuttoin',filterButton,option);
             if(filterButton){
                 filterButton.click();
             }
