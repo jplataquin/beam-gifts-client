26a02eb4f02e0bbf4ac8d51bcca3053c274bd6f5
jplataquin <jp.lataquin@gmail.com>
2023-03-03 20:30:34 +0800

    test

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -3,47 +3,48 @@
 @section('content')
 
 <div class="container">
-    <h1>Frequenty Asked Questions (FAQ)</h1>
+    <h1 class="mt-3 ">Frequenty Asked Questions (FAQ)</h1>
     
 
     <div class="mb-3 bg-white p-3 rounded border-darkmagenta">
         <div class="mb-3 fw-bold fs-4">
-            Q: How to claim a E-gift?
+            Q: How do you claim an E-gift?
         </div>    
         <div class="fs-5">
-            A: The receiver of the E-gift can present the QR code to the establishment in order to claim the item. 
-            For more details please check the "<a href="/how-to-claim">how to claim</a>" page.
+            A: The recipient shall present the QR code to the establishment before the gift is released.
+            
+            For more details on how to claim please visit the "<a href="/how-to-claim">how to claim</a>" page.
         </div>
     </div>
 
 
     <div class="mb-3 bg-white p-3 rounded border-darkmagenta">
         <div class="mb-3 fw-bold fs-4">
-            Q: What happens if I sent the E-gift to the wrong person?
+            Q: What if I sent the gift to the wrong person?
         </div>    
         <div class="fs-5">
-            A: Unfortnately the responsiblity of securing and sending the E-gift falls in the hands of the user who bought and sent it.
-            Currently Beam Gifts does not have any password protection or security measure to prevent such mistake from happening. 
+            A: The person who bought the gift is solely responsible for securing and correctly sending the link to the right person.
+            There is currently no feature to retrieve or void the link in case the gift was sent to the wrong person. 
         </div>
     </div>
     
     <div class="mb-3 bg-white p-3 rounded border-darkmagenta">
         <div class="mb-3 fw-bold fs-4">
-            Q: Is it safe to share my E-gift QR code publicly or online?
+            Q: Is it safe to share my gift link (QR Code) publicly?
         </div>    
         <div class="fs-2">
-            A: No, please keep the QR code confidential from other people. 
-            Sharing the QR code publicly or online can result in someone gaining control of the E-gift.
+            A: No, please keep the link or QR code confidential. 
+            Sharing the link or QR code publicly can result in the gift being stolen.
         </div>
     </div>
 
 
     <div class="mb-3 bg-white p-3 rounded border-darkmagenta">
         <div class="mb-3 fw-bold fs-4">
-            Q: What happens if the E-gift I want to claim is not available or is out of stock.
+            Q: What happens if the gift I want to claim is not available or is out of stock.
         </div>    
         <div class="fs-5">
-            A: All E-gift items can be substituted for another item with equal or lesser value upon cliaming. 
+            A: All gift items can be substituted for another item with equal or lesser value upon cliaming. 
             Note that no monetary change is given for items with lesser value.
         </div>
     </div>
@@ -51,21 +52,21 @@
 
     <div class="mb-3 bg-white p-3 rounded border-darkmagenta">
         <div class="mb-3 fw-bold fs-4">
-            Q: Can I request to change the QR code of the E-gift?
+            Q: Can I request to change the QR code of my gift link?
         </div>    
         <div class="fs-5">
-            A: No, Each E-gift has a unique QR code and once generated it cannot be changed.
+            A: No, Each gift has a unique QR code and once generated it cannot be changed.
         </div>
     </div>
     
     
     <div class="mb-3 bg-white p-3 rounded border-darkmagenta">
         <div class="mb-3 fw-bold fs-4">
-            Q: How do I refund a E-gift item?
+            Q: How do I refund a gift item?
         </div>    
         <div class="fs-5">
-            A: Only the user who bought the E-gift can request for a refund. 
-            In order to initiate the process, please email us using your registered email at "refund@beam.gifts".
+            A: Only the user who bought the gift can request for a refund. 
+            In order to initiate the process, please email us using your registered email to "refund@beam.gifts".
             Note that refunds are subject to our <a href="/tos">terms of service</a>, and due to third party payment providers we will not be able to refund
             the entire amount.
         </div>
@@ -74,11 +75,11 @@
 
     <div class="mb-3 bg-white p-3 rounded border-darkmagenta">
         <div class="mb-3 fw-bold fs-4">
-            Q: What happens if the brand or service is no longer available and my E-gift has not yet been claimed.
+            Q: What happens if the brand or establishment is no longer existing.
         </div>    
         <div class="fs-5">
-            A: In the event that the brand or service has closed down or no longer exists, the user who bought the E-gift can request for a refund.
-            Note that the E-gift must not be expired and is subject to the conditions in our <a href="/tos">terms of service</a>,.
+            A: In the event that the brand or establishment has closed down or no longer exists, the user who bought the gift can request for a refund.
+            Note that the link must not be expired and shall be subject to our <a href="/tos">terms of service</a>,.
         </div>
     </div>
 
