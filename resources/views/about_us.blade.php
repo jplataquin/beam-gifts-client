fa84ac8767a3b46f4ac611f64e9505769307411d
jplataquin <jp.lataquin@gmail.com>
2023-03-16 18:42:19 +0800

    test

--- resources/views/about_us.blade.php
+++ resources/views/about_us.blade.php
@@ -5,10 +5,10 @@
 <div class="container">
     <h1>About Us</h1>
     <hr>
-    <div style="padding:20px; background-color:#FFFFFF; border-radius: 25px;font-size:20px;margin-bottom:10px;text-align: justify;padding-left:20px; padding-right:20px">
+    <div style="padding:20px; background-color:#FFFFFF; border-radius: 25px;font-size:20px;margin-bottom:10px;text-align: justify;">
         <p>Hello There!</p>
 
-        <p style="text-indent:1cm;">
+        <p>
             "Beam Gifts" is a convenient platform for sending and receiving a gifts through the internet.
             We provide gifts for any occasion that will bring joy to your love ones. 
         </p>
