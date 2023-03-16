d24c95ac562b6b451540aea184869084f6d361dd
jplataquin <jp.lataquin@gmail.com>
2023-03-16 18:21:28 +0800

    test

--- resources/views/brands.blade.php
+++ resources/views/brands.blade.php
@@ -41,9 +41,7 @@
 
                                 
                                 <div class="col-12 text-start">
-
-                                <div class="col-12 text-start" style="z-index:-2">
-                                    <div class="input-group"> 
+                                    <div class="input-group" style="z-index:-1"> 
                                         <span class="input-group-text">
                                             <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                 <path opacity="0.1" d="M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" fill="#323232"/>
@@ -51,6 +49,7 @@
                                                 <path d="M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke="#323232" stroke-width="2"/>
                                             </svg>
                                         </span>
+
                                         <input id="query" type="text" class="form-control" placeholder="Search Brand"/>
                                     </div>
                                 </div>
