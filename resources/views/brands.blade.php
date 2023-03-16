57ac1b3f8826c6d935a8d8d96729644e2a501c80
jplataquin <jp.lataquin@gmail.com>
2023-03-16 18:20:01 +0800

    test

--- resources/views/brands.blade.php
+++ resources/views/brands.blade.php
@@ -10,7 +10,7 @@
                         <h2 class="title">Brands</h2>
                         <div class="title-bg"></div>
                     </div>
-                    <div class="subTitle">See our partnered brands ❤️</div>
+                    <div class="subTitle">See our partners ❤️</div>
                 </div>
             </div>
             <div class="row mt-5">
@@ -38,8 +38,21 @@
                                  <p class="totals">Total 15</p> 
                                 </div>
                                 -->
+
+                                
                                 <div class="col-12 text-start">
-                                    <input id="query" type="text" class="form-control" placeholder="Search Brand"/>
+
+                                <div class="col-12 text-start" style="z-index:-1">
+                                    <div class="input-group"> 
+                                        <span class="input-group-text">
+                                            <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path opacity="0.1" d="M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" fill="#323232"/>
+                                                <path d="M15 15L21 21" stroke="#323232" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                                <path d="M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke="#323232" stroke-width="2"/>
+                                            </svg>
+                                        </span>
+                                        <input id="query" type="text" class="form-control" placeholder="Search Brand"/>
+                                    </div>
                                 </div>
                             </div>
                             <div class="row mt-5">
